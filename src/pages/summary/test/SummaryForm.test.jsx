f944ee5a5dfcf8aea99be4983d7b1262f8cf26c1
truejojo <johannesveh@yahoo.de>
2022-11-05 14:36:23 +0100

    add functionality and tests of overlay

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { render, fireEvent, screen, getByRole } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 describe("Summary Form", () => {
@@ -14,18 +15,40 @@ describe("Summary Form", () => {
     expect(confirmButton).toBeDisabled();
   });
 
-  test("checkbox enables button on first click and disables on second click", () => {
+  test("checkbox enables button on first click and disables on second click", async () => {
     render(<SummaryForm />);
+    const user = userEvent.setup();
 
     const checkbox = screen.getByRole("checkbox", {
       name: "I agree to Terms and Conditions",
     });
     const confirmButton = screen.getByRole("button", { name: "Confirm order" });
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(confirmButton).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(confirmButton).toBeDisabled();
+  });
+
+  test("popover responds to hover", async () => {
+    render(<SummaryForm />);
+    const user = userEvent.setup();
+
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivere/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears on mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(/no ice cream will actually be delivere/i);
+    expect(popover).toBeInTheDocument();
+
+    // popover disappears when we mouse out
+    await user.unhover(termsAndConditions);
+    expect(popover).not.toBeInTheDocument();
   });
 });
