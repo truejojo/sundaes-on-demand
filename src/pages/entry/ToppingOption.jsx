02597088f2a877999d5551e0932ffea6d7ee6ddc
truejojo <johannesveh@yahoo.de>
2022-11-08 15:31:49 +0100

    add test for toppings and logic with react for the page

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 const ToppingOption = ({ name, imagePath }) => {
+  const { updateItemCount } = useOrderDetails();
+
+  const handleChange = (e) =>
+    updateItemCount(name, e.target.checked ? 1 : 0, "toppings");
+
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -9,8 +16,11 @@ const ToppingOption = ({ name, imagePath }) => {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`} >
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
+      </Form.Group>
     </Col>
-  )
-}
+  );
+};
 
-export default ToppingOption
+export default ToppingOption;
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -3,7 +3,7 @@ import userEvent from "@testing-library/user-event";
 import Options from "../Options";
 
 test("update scoop subtotal when scoops change", async () => {
-  render(<Options optionType="scoops" /> );
+  render(<Options optionType="scoops" />);
 
   // make sure total starts out $0.00
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
@@ -24,4 +24,36 @@ test("update scoop subtotal when scoops change", async () => {
   await userEvent.clear(chocolateInput);
   await userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
+
+test("update topping subtotal when toppings change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  // make sure total starts out $0.00
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  // add cherries toppings and check the subtotal
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("1.50");
+
+  // add M&Ms toppings and check the subtotal
+  const m_msCheckbox = await screen.findByRole("checkbox", {
+    name: "M&Ms",
+  });
+  await user.click(m_msCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("3.00");
+
+  // add cherries toppings and check the subtotal
+  const hot_fudgeCheckbox = await screen.findByRole("checkbox", {
+    name: "Hot fudge",
+  });
+  await user.click(hot_fudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent("4.50");
 });
